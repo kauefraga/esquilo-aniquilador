4f5134758e05add4f6ebb419a6017d26945e75b6
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2024-03-01 18:50:49 -0300

    scripts: create a script that removes all containers and compose it again

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #! /usr/bin/env bash
 
+docker rm esquilo-aniquilador-nginx-1
+docker rm esquilo-aniquilador-api01-1
+docker rm esquilo-aniquilador-api02-1
 docker rm -v esquilo-aniquilador-db-1
 docker compose up
