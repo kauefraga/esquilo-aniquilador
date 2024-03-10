283f8ac150811e6cb6c621dab1385cce974e0e1c
Kauê Fraga Rodrigues <rkauefraga@gmail.com>
2024-03-10 14:28:58 -0300

    config: change 'transacoes' table to unlogged table, disable autovacuum and create an index in the 'transacoes' table

--- init.sql
+++ init.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS clientes (
   saldo  INTEGER NOT NULL DEFAULT 0
 );
 
-CREATE TABLE IF NOT EXISTS transacoes (
+CREATE UNLOGGED TABLE IF NOT EXISTS transacoes (
   id           SERIAL PRIMARY KEY,
   valor        INTEGER NOT NULL,
   tipo         CHAR(1) NOT NULL,
@@ -13,6 +13,13 @@ CREATE TABLE IF NOT EXISTS transacoes (
   cliente_id   INTEGER NOT NULL,
   realizada_em TIMESTAMP NOT NULL DEFAULT NOW()
 );
+
+ALTER TABLE
+  transacoes
+  SET
+    (autovacuum_enabled = false);
+
+CREATE INDEX idx_transacoes ON transacoes (cliente_id);
 
 INSERT INTO clientes (nome, limite)
 VALUES
